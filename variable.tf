d83e3733faddcf54955870b46c3ba0989ffeddd2
Rahul Rawat <81628422+RRrawat@users.noreply.github.com>
2022-08-04 09:06:30 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,107 +1,49 @@
 variable "region" {
     type = string
     default = "us-east-1"
-    description = "AWS region for the service"
 }
-variable "enabled" {
-  type    = bool
-  default = true
+variable "eks_cluster_id" {
+    type = string
+    default = " "
 }
 
-variable "cluster_name" {
-  type        = string
-  description = "The name of the EKS cluster"
+variable "efs_csi_service_account_name" {
+    type = string
+    default = "efs-csi-driver-service-account"
 }
 
 variable "cluster_identity_oidc_issuer" {
-  type        = string
-  description = "The OIDC Identity issuer for the cluster."
+    type = string
+    default = " "
 }
 
 variable "cluster_identity_oidc_issuer_arn" {
-  type        = string
-  description = "The OIDC Identity issuer ARN for the cluster that can be used to associate IAM roles with a service account."
+    type = string
+    default = " "
 }
 
-variable "helm_chart_name" {
-  type        = string
-  default     = "aws-efs-csi-driver"
-  description = "Amazon EFS CSI Driver chart name."
+variable "efs_csi_role_name" {
+    type= string
+    default = "aws-eks-efs-csi-driver-role"
 }
 
-variable "helm_chart_release_name" {
-  type        = string
-  default     = "aws-efs-csi-driver"
-  description = "Amazon EFS CSI Driver release name."
+variable "efs_csi_policy_name" {
+    type= string
+    default = "aws-eks-efs-csi-driver-policy"
 }
 
-variable "helm_chart_repo" {
-  type        = string
-  default     = "https://kubernetes-sigs.github.io/aws-efs-csi-driver/"
-  description = "Amazon EFS CSI Driver repository name."
-}
-
-variable "helm_chart_version" {
-  type        = string
-  default     = "2.2.0"
-  description = "Amazon EFS CSI Driver chart version."
-}
-
-variable "create_namespace" {
-  type        = bool
-  default     = true
-  description = "Whether to create k8s namespace with name defined by `namespace`."
-}
-
-variable "namespace" {
-  type        = string
-  default     = "efs-csi-driver"
-  description = "Kubernetes namespace to deploy EFS CSI Driver Helm chart."
-}
-
-variable "service_account_name" {
-  type        = string
-  default     = "aws-efs-csi-driver"
-  description = "Amazon EFS CSI Driver service account name."
+variable "namespace"{
+    type = string
+    default = "efs-csi-driver"
+    
 }
 
 variable "storage_class_name" {
-  type        = string
-  default     = "efs-sc"
-  description = "Storage class name for EFS CSI driver."
+    type =string
+    default = "efs-csi-storage"
 }
 
-variable "create_storage_class" {
-  type        = bool
-  default     = true
-  description = "Whether to create Storage class for EFS CSI driver."
-}
-
-variable "settings" {
-  default     = {}
-  description = "Additional settings which will be passed to the Helm chart values, see https://github.com/kubernetes-sigs/aws-efs-csi-driver."
-}
-/**********************************************************
-* Common tags variable 
-*************************************************************/
-
-
-variable "name" {
-	type = string
-	description = "Name of the provider resource"
-}
-variable "stack" {
-    type = string
-	description	= "Provide resource stack"
-}
-
-variable "enviroment" {
-    type = string
-	description	= "Provide environment (DEV/TEST/PRD)"
-}
-
-variable "additional_tags"{
-	type = map(string)
-	default = null
-	description = "If user wants to add any other additional tags to resource"
+variable "enabled" {
+    type = bool
+    default = true
 }
